0560cb828d963210c47ce2626df024fa678e9c07
shubhanshu jain <shubhanshujain997@gmail.com>
2022-09-25 19:19:38 +0530

    practise

--- DFS/src/without_destination/Graph.java
+++ DFS/src/without_destination/Graph.java
@@ -2,36 +2,62 @@ package without_destination;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
 class Graph {
-	  private LinkedList<Integer> adjLists[];
+	  private LinkedList<Integer> adj[];
 	  private boolean visited[];
 
 	  // Graph creation
 	  Graph(int vertices) {
-	    adjLists = new LinkedList[vertices];
-	    visited = new boolean[vertices];
+	    adj = new LinkedList[vertices];
+
 
 	    for (int i = 0; i < vertices; i++)
-	      adjLists[i] = new LinkedList<Integer>();
+	      adj[i] = new LinkedList<Integer>();
 	  }
 
 	  // Add edges
 	  void addEdge(int src, int dest) {
-	    adjLists[src].add(dest);
+	    adj[src].add(dest);
 	  }
 
 	  // DFS algorithm
 	  void DFS(int vertex) {
+		   visited = new boolean[adj.length];
 	    visited[vertex] = true;
 	    System.out.print(vertex + " ");
 
-	    Iterator<Integer> ite = adjLists[vertex].listIterator();
+	    Iterator<Integer> ite = adj[vertex].listIterator();
 	    while (ite.hasNext()) {
 	      int neighbour = ite.next();
 	      if (!visited[neighbour])
 	        DFS(neighbour);
 	    }
+	  }
+	  
+	  public void DFSstack(int source)
+	  {
+		  boolean [] vis =new boolean[adj.length];
+		  Stack<Integer> stack = new Stack();
+		  stack.push(source);
+		  System.out.println();
+		  while(!stack.isEmpty())
+		  {
+			  int cur=stack.pop();
+			  System.out.print(cur+"->");
+			  
+			  for(int neighbour : adj[source])
+			  {
+				  if(!vis[neighbour])
+					{
+					 vis[neighbour]=true;
+					 stack.push(neighbour);
+				  }
+			  }
+			  
+		  }
+ 		  
 	  }
 
 	  public static void main(String args[]) {
@@ -45,5 +71,6 @@ class Graph {
 	    System.out.println("Following is Depth First Traversal");
 
 	    g.DFS(2);
+	    g.DFSstack(2);
 	  }
 	}
